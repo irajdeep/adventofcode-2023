ca6cdca3271c06fe6f34231af1c042ae2f35a958
Rajdeep Das <irajdeep94@gmail.com>
2023-12-29 09:39:11 +0530

    day12 : 2

--- day12.py
+++ day12.py
@@ -1,10 +1,33 @@
+import copy
+
 def allq(ss) -> bool:
     for ch in ss:
         if not (ch == "?" or ch == "#"): return False
     return True
 
-def dp(ss, l, arr, posa) -> int:
-    # print(l, posa)
+def extent_s(s: str) -> str:
+    curr = s
+    n = 1
+
+    while n <= 4:
+        s += "?"
+        s += curr
+        n += 1
+
+    return s
+
+
+def extend_arr(arr: list[int]) -> list[int]:
+    curr = copy.deepcopy(arr)
+    n = 1
+
+    while n <= 4:
+        arr.extend(curr)
+        n += 1
+    return arr
+
+
+def dp(ss, l, arr, posa, DP) -> int:
     if l >= len(ss):
         return posa >= len(arr)
 
@@ -12,18 +35,21 @@ def dp(ss, l, arr, posa) -> int:
        if "#" not in ss[l:]: return True
        return False
 
+    if(l, posa) in DP: return DP[(l, posa)]
+
     ans = 0
     count = arr[posa]
     r = l + count
 
 
     if r <= len(ss) and allq(ss[l : r]) and (r >= len(ss) or ss[r] != "#"):
-        ans += dp(ss, r + 1, arr, posa + 1)
+        ans += dp(ss, r + 1, arr, posa + 1, DP)
 
     if ss[l] != "#":
-        ans += dp(ss, l + 1, arr, posa)
+        ans += dp(ss, l + 1, arr, posa, DP)
 
-    return ans
+    DP[(l, posa)] = ans
+    return DP[(l, posa)]
 
 def solve1(path: str) -> int:
     with open(path) as file:
@@ -34,7 +60,9 @@ def solve1(path: str) -> int:
     for line in lines:
         ss, nums = line.split(" ")
         nums = [int(x) for x in nums.split(",")]
-        ans += dp(ss, 0, nums, 0)
+        # print(extent_s(ss), extend_arr(nums))
+        DP = {}
+        ans += dp(extent_s(ss), 0, extend_arr(nums), 0, DP)
 
     return ans
 
